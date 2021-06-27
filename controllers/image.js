d436b14c13b91d8e937d8faabe195dded97989cb
michael23-lopes <michael23.wellington@gmail.com>
2021-06-27 18:20:17 -0300

    smart-brain-api

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = require('clarifai');
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+  apiKey: 'e2ea32dc78c24b7093832b2045af6d11',
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json('unable to work with API'));
+};
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => res.status(400).json('unable to get count'));
+};
+
+module.exports = {
+  handleImage: handleImage,
+  handleApiCall: handleApiCall,
+};
